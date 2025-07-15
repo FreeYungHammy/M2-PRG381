ca3e122d372b1e608d37a1f8fa4dde374d79aeca
Rourke <rourke@nitida.co.za>
2025-07-15 14:33:32 +0200

    Counselor and Feedback object

--- src/PRG381_Milestone2/model/Counselor.java
+++ src/PRG381_Milestone2/model/Counselor.java
@@ -4,10 +4,44 @@
  */
 package PRG381_Milestone2.model;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 /**
  *
  * @author calvi
  */
 public class Counselor {
     
+    private String name;
+    private String specialization;
+    private boolean available;
+    
+    public Counselor(String name, String specialization, boolean available)
+    {
+        this.name = name;
+        this.specialization = specialization;
+        this.available = available;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName) {
+        this.name = newName;
+    }
+    
+    public String getSpec() {
+        return specialization;
+    }
+    public void setSpec(String newSpec) {
+        this.specialization = newSpec;
+    }
+    
+    public boolean getAvailable() {
+        return available;
+    }
+    public void setAvailable(boolean newAvailable) {
+        this.available = newAvailable;
+    }
 }
--- src/PRG381_Milestone2/model/Feedback.java
+++ src/PRG381_Milestone2/model/Feedback.java
@@ -9,5 +9,43 @@ package PRG381_Milestone2.model;
  * @author calvi
  */
 public class Feedback {
+    private String name;
+    private int rating;
+    private String comments;
     
+    public Feedback (String name, int rating, String comments)
+    {
+        this.name = name;
+        this.rating = rating;
+        this.comments = comments;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+    
+    public int getRating() 
+    {
+        return rating;
+    }
+    public void setRating(int newRating)
+    {
+        this.rating = newRating;
+    }
+    
+    public String getComments()
+    {
+        return comments;
+    }
+    public void setComments(String newComments)
+    {
+        this.comments = newComments;
+    }
 }
+
+
