0e31436dd900389bb9f29befb1ee7c966eb393e6
Rourke <rourke@nitida.co.za>
2025-07-15 14:04:36 +0200

    Appointment object

--- src/PRG381_Milestone2/model/Appointment.java
+++ src/PRG381_Milestone2/model/Appointment.java
@@ -4,10 +4,53 @@
  */
 package PRG381_Milestone2.model;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 /**
  *
  * @author calvi
  */
-public abstract class Appointment {
+public class Appointment {
+    private String counselorName;
+    private Date appDate;
+    private LocalTime appTime;
+    private String appStatus;
+    
+    public Appointment(String counselorName, Date appDate, LocalTime appTime, String appStatus)
+    {
+        this.counselorName = counselorName;
+        this.appDate = appDate;
+        this.appTime = appTime;
+        this.appStatus = appStatus;
+    }
+    
+    public Date getDate() {
+        return appDate;
+    }
+    public void setDate(Date newDate) {
+        this.appDate = newDate;
+    }
+
+    public LocalTime getTime() {
+        return appTime;
+    }
+    public void setTime(LocalTime newTime) {
+        this.appTime = newTime;
+    }
+    
+    public String getConName() {
+        return counselorName;
+    }
+    public void setConName(String newConName) {
+        this.counselorName = newConName;
+    }
+    
+    public String getStatus() {
+        return appStatus;
+    }
+    public void setStatus(String updateStatus) {
+        this.appStatus = updateStatus;
+    }
     
 }
