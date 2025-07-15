fb92323952c2ea6571102de330a8c58cba2ca2c6
Rourke <rourke@nitida.co.za>
2025-07-15 12:37:56 +0200

    testing

--- src/PRG381_Milestone2/model/Appointment.java
+++ src/PRG381_Milestone2/model/Appointment.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package PRG381_Milestone2.model;
 
 /**
  *
  * @author calvi
  */
-public class Appointment {
+public abstract class Appointment {
     
 }
