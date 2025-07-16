25dee935ddc7511556a572c366c25920f90a41b1
600373 <600373@bcavdshsan-20.avd.belgiumcampus.ac.za>
2025-07-16 10:20:24 +0200

    Creating logic for appointment controller.

--- src/PRG381_Milestone2/controller/AppointmentController.java
+++ src/PRG381_Milestone2/controller/AppointmentController.java
@@ -5,6 +5,7 @@
 package PRG381_Milestone2.controller;
 
 import PRG381_Milestone2.model.Appointment;
+import java.sql.*;
 import java.time.LocalTime;
 import java.util.Date;
 
@@ -13,6 +14,33 @@ import java.util.Date;
  * @author calvi
  */
 public class AppointmentController {
+    
+    private final Connection con;
+    
+    public AppointmentController(){
+        con = new DBConnection.getConnection();
+        createTableIfNotExists();
+    }
+    
+    private void createTableIfNotExists(){
+        String sql = "CREATE TABLE Appointment (" +
+                "id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY , " +
+                "counselor VARCHAR(100), " +
+                "date DATE, " + 
+                "time VARCHAR(20), "+ 
+                "status VARCHAR(50))";
+        
+        try(Statement stmt = con.createStatement()){
+            stmt.executeUpdate(sql);
+            System.out.println("Appointment Table Created");
+        } catch(SQLException e){
+            if(e.getSQLState().equals("X0Y32")){
+                System.out.println("Appointment table already exists.");
+            } else {
+              System.out.println("Error creating table: " + e.getMessage());
+            }
+        }
+    }
     
     //use form input as method input
     public void bookAppointment(String counselor, Date date, LocalTime time)
@@ -23,7 +51,7 @@ public class AppointmentController {
         Appointment newApp = new Appointment(counselor, date, time, "upcoming");
 
         //update list adn then maybe update database or only update database at end of operation
-        
+        insertAppointment(newApp);
     }
     
     public Appointment updateApp(Appointment appChanged, String counselor, Date date, LocalTime time)
@@ -34,5 +62,20 @@ public class AppointmentController {
         appChanged.setTime(time);
         
         return appChanged;
+    }
+    
+    public void insertAppointment(Appointment app){
+        String sql = "INSERT INTO Appointment (counselor, date, time, status) VALUES (?, ?, ?, ?)";
+        try(PreparedStatement stmt = con.prepareStatement(sql)){
+            stmt.setString(1, app.getCounName());
+            stmt.setDate(2, new java.sql.Date(app.getDate).getTime());
+            stmt.setString(3, app.getTime().toString());
+            stmt.setString(4, app.getStatus());
+            
+            stmt.executeUpdate();
+            System.out.print("Appointment Inserted into database.");
+        } catch (SQLException e){
+            System.out.println("Error inserting into database");
+        }
     }
 }
